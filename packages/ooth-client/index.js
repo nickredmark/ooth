8a24f1dbe0e0017cc305e3f715d59c9a16e94df8
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-02-28 16:07:33 +0100

    ooth client standalone

--- packages/ooth-client/index.js
+++ packages/ooth-client/index.js
@@ -3,12 +3,16 @@ const fetch = require('isomorphic-fetch')
 class OothClient {
     constructor({
         oothUrl,
+        standalone,
         apiLoginUrl,
         apiLogoutUrl
     }) {
         this.oothUrl = oothUrl
-        this.apiLoginUrl = apiLoginUrl
-        this.apiLogoutUrl = apiLogoutUrl
+        this.standalone = standalone
+        if (standalone) {
+            this.apiLoginUrl = apiLoginUrl
+            this.apiLogoutUrl = apiLogoutUrl
+        }
     }
     authenticate(strategy, method, body) {
         return fetch(`${this.oothUrl}/${strategy}/${method}`, {
@@ -16,17 +20,20 @@ class OothClient {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: body && JSON.stringify(body)
+            body: body && JSON.stringify(body),
+            credentials: 'include'
         }).then(response => {
-            return response.json()
-        }).then(({token}) => {
-            return fetch(this.apiLoginUrl, {
-                method: 'POST',
-                headers: {
-                    'Authorization': `JWT ${token}`
-                },
-                credentials: 'include'
-            })
+            if (this.standalone) {
+                return response.json().then(({token}) => {
+                    return fetch(this.apiLoginUrl, {
+                        method: 'POST',
+                        headers: {
+                            'Authorization': `JWT ${token}`
+                        },
+                        credentials: 'include'
+                    })
+                })
+            }
         })
     }
     method(strategy, method, body) {
@@ -35,15 +42,31 @@ class OothClient {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(body)
+            body: JSON.stringify(body),
+            credentials: 'include'
         }).then(response => {
             return response.json()
         })
     }
     logout() {
-        return fetch(this.apiLogoutUrl, {
+        return fetch(`${this.oothUrl}/logout`, {
             method: 'POST',
             credentials: 'include'
+        }).then(response => {
+            if (this.standalone) {
+                return fetch(this.apiLogoutUrl, {
+                    method: 'POST',
+                    credentials: 'include'
+                })
+            }
+        })
+    }
+    status() {
+        return fetch(`${this.oothUrl}/status`, {
+            method: 'GET',
+            credentials: 'include'
+        }).then(response => {
+            return response.json()
         })
     }
 }
