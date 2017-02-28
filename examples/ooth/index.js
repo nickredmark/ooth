4208b17e752bbb9c2111b6e49487cadf855cb230
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-02-28 16:03:31 +0100

    update ooth example

--- examples/ooth/index.js
+++ examples/ooth/index.js
@@ -8,6 +8,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const mail = require('./mail')
 const settings = require('./settings')
+const session = require('express-session')
 
 async function start() {
 
@@ -15,13 +16,26 @@ async function start() {
 
         const app = express()
         app.use(morgan('dev'))
-        app.use(cors())
+        const corsMiddleware = cors({
+            origin: settings.originUrl,
+            credentials: true,
+            preflightContinue: false
+        })
+        app.use(corsMiddleware)
+        app.options(corsMiddleware)
+
+        app.use(session({
+            name: 'ooth-session-id',
+            secret: settings.sessionSecret,
+            resave: false,
+            saveUninitialized: true,
+        }))
 
         const ooth = new Ooth({
+            standalone: true,
             mongoUrl: settings.mongoUrl,
             sharedSecret: settings.sharedSecret
         })
-
         await ooth.start(app)
 
         ooth.use('guest', oothGuest())
--- examples/ooth/settings.dist.js
+++ examples/ooth/settings.dist.js
@@ -1,6 +1,7 @@
 module.exports = {
     port: 3001,
     mongoUrl: 'mongodb://localhost:27017/ooth',
+    originUrl: 'http://localhost:3000',
     mailgun: {
         apiKey: "XXX",
         domain: "XXX"
@@ -9,4 +10,5 @@ module.exports = {
         from: "info@example.com"
     },
     sharedSecret: 'XXX', // Has to be the same in api server
+    sessionSecret: 'XXX'
 }
