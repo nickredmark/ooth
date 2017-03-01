145f3a1095bff583f3479130fb58c408e5e40135
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-03-01 17:56:20 +0100

    Ooth client react: subscribe to changes to user

--- packages/ooth-client-react/index.js
+++ packages/ooth-client-react/index.js
@@ -1,17 +1,45 @@
 const React = require('react')
-const {withContext, getContext} = require('recompose')
+const {getContext} = require('recompose')
 
-const ID = ({children}) => React.Children.only(children)
-
-const OothProvider = withContext({
-    oothClient: React.PropTypes.object.isRequired
-}, ({client: oothClient}) => ({oothClient}))(ID)
+class OothProvider extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: null
+        }
+        props.client.user().subscribe(user => {
+            this.setState({
+                user
+            })
+        })
+    }
+    render() {
+        return React.Children.only(this.props.children)
+    }
+    getChildContext() {
+        const {client: oothClient} = this.props
+        const {user} = this.state
+        return {
+            oothClient,
+            user
+        }
+    }
+}
+OothProvider.childContextTypes = {
+    oothClient: React.PropTypes.object.isRequired,
+    user: React.PropTypes.object
+}
 
 const withOoth = getContext({
     oothClient: React.PropTypes.object.isRequired
 })
 
+const withUser = getContext({
+    user: React.PropTypes.object
+})
+
 module.exports = {
     OothProvider,
-    withOoth
+    withOoth,
+    withUser
 }
