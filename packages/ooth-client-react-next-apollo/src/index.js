052a88d99cad8debc53d085244864bbaacb0b52a
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-07-18 14:09:12 +0200

    add ooth-client-react-next-apollo

---
+++ packages/ooth-client-react-next-apollo/src/index.js
@@ -0,0 +1,137 @@
+import ApolloClient, {createNetworkInterface} from 'apollo-client'
+import {ApolloProvider, getDataFromTree} from 'react-apollo'
+import React from 'react'
+import {defaultProps} from 'recompose'
+import { createStore as createReduxStore, combineReducers, applyMiddleware } from 'redux'
+
+let client = null
+let store = null
+
+const createClient = (uri) => {
+    const networkInterface = createNetworkInterface({
+        uri,
+        opts: {
+            credentials: 'include'
+        }
+    })
+    return new ApolloClient({
+        networkInterface
+    })
+}
+
+const createSSRClient = (uri, cookies) => {
+    const networkInterface = createNetworkInterface({
+        uri
+    })
+    networkInterface.use([{
+        applyMiddleware(req, next) {
+            if (!req.options.headers) {
+                req.options.headers = {}
+            }
+            req.options.headers['Cookie'] = Object.keys(cookies).map(key => `${key}=${cookies[key]}`).join('; ')
+            next()
+        }
+    }])
+    return new ApolloClient({
+        ssrMode: !process.browser,
+        networkInterface
+    })    
+}
+
+const initClient = (uri, cookies) => {
+    if (!process.browser) {
+        // on server, create a new client for each request
+        return createSSRClient(uri, cookies)
+    } else {
+        // on client, create singleton
+        if (!client) {
+            client = createClient(uri)
+        }
+        return client
+    }
+}
+
+const createStore = (client, initialState) => {
+    return createReduxStore(
+        combineReducers({
+            apollo: client.reducer()
+        }),
+        initialState,
+        applyMiddleware(client.middleware())
+    )
+}
+
+const initStore = (client, initialState) => {
+    if (!process.browser) {
+        // on server, create a new store for each request
+        return createStore(client, initialState)
+    } else {
+        // on client, create singleton
+        if (!store) {
+            store = createStore(client, initialState)
+        }
+        return store
+    }
+}
+
+module.exports = ({url}) => {
+    return (Component) => (
+        class extends React.Component {
+            static getInitialProps (ctx) {
+                const cookies = ctx.req && ctx.req.cookies
+                const client = initClient(url, cookies)
+                const store = initStore(client, client.initialState)
+
+                return Promise.resolve(Component.getInitialProps ? Component.getInitialProps(ctx) : {})
+                    .then(props => {
+                        return Promise.resolve()
+                            .then(() => {
+                                if (!process.browser) {
+                                    const url = {
+                                        query: ctx.query,
+                                        pathname: ctx.pathname,
+                                        asPath: ctx.asPath
+                                    }
+                                    const app = (
+                                        <ApolloProvider client={client} store={store}>
+                                            <Component url={url} {...props} />
+                                        </ApolloProvider>
+                                    )
+                                    return getDataFromTree(app)
+                                }
+                            })
+                            .then(() => {
+                                const state = store.getState()
+
+                                return Object.assign({
+                                    initialState: Object.assign(
+                                        {},
+                                        state,
+                                        {
+                                            apollo: {
+                                                data: client.getInitialState().data
+                                            }
+                                        }
+                                    )
+                                }, props)
+                            })
+                    })
+            }
+
+            constructor (props) {
+                super(props)
+                // On server, this client won't be doing any work, because all work has been done in getInitialProps
+                this.client = initClient(url)
+                this.store = initStore(this.client, props.initialState)
+            }
+
+            render () {
+                return (
+                    <ApolloProvider client={this.client} store={this.store}>
+                        <Component {...this.props} />
+                    </ApolloProvider>
+                )
+            }
+        }
+    )
+}
