f9b327ae1e5973f72fd958c8cb4d0cfba5b2ee5f
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-10-04 09:04:30 +0200

    ooth-google use passport connect method

--- packages/ooth-google/src/index.js
+++ packages/ooth-google/src/index.js
@@ -1,11 +1,4 @@
-const nodeify = require('nodeify')
 const GoogleStrategy = require('passport-google-id-token')
-
-function nodeifyAsync(asyncFunction) {
-    return function(...args) {
-        return nodeify(asyncFunction(...args.slice(0, -1)), args[args.length - 1])
-    }
-}
 
 module.exports = function({
     clientID,
@@ -15,38 +8,17 @@ module.exports = function({
         name,
         registerUniqueField,
         registerProfileField,
-        registerPassportMethod,
-        requireNotLogged,
-        getUserById,
-        getUserByUniqueField,
-        insertUser,
-        updateUser,
+        registerPassportConnectMethod,
     }) {
         registerUniqueField('email', 'email')
         registerProfileField('email')
-        registerPassportMethod('login', requireNotLogged, new GoogleStrategy({
+        registerPassportConnectMethod('login', new GoogleStrategy({
             clientID,
             clientSecret,
-        }, nodeifyAsync((parsedToken, googleId) => {
-            const email = parsedToken.payload.email
-            return getUserByUniqueField('email', email)
-                .then(user => {
-                    if (user) {
-                        if (!user[name]) {
-                            return updateUser(user._id, {
-                                email,
-                            }).then(() => user)
-                        }
-
-                        return user
-                    }
-
-                    return insertUser({
-                        email,
-                    }).then(_id => {
-                        return getUserById(_id)
-                    })
-                })
-        })))
+        }, (parsedToken, googleId, done) => {
+            return done(null, {
+                email: parsedToken.payload.email
+            })
+        }))
     }
 }
