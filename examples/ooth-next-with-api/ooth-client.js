1db50b7df37b73378c845817b21d093187cca920
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-11-13 16:43:50 +0100

    remove outdated ooth-next-with-api example

--- examples/ooth-next-with-api/ooth-client.js
+++
@@ -1,9 +0,0 @@
-import OothClient from 'ooth-client'
-import withOothNext from 'ooth-client-react-next'
-const Rx = require('rx')
-
-const oothClient = new OothClient({
-    oothUrl: 'http://localhost:3002/auth',
-})
-
-export default withOothNext(oothClient)
