d4c07cbf18f79612f92a56087e3e53618be69c8b
izaleu <steel.princess@gmail.com>
2018-01-07 15:42:25 -0800

    Modify ooth/index.js to use strategies from strategies.js

--- examples/standalone/ooth/index.js
+++ examples/standalone/ooth/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const Ooth = require('ooth')
 const OothMongo = require('ooth-mongo')
-const oothGuest = require('ooth-guest')
-const oothLocal = require('ooth-local')
+
 const morgan = require('morgan')
 const cors = require('cors')
 const mail = require('./mail')
 const settings = require('./settings')
 const session = require('express-session')
 const {MongoClient, ObjectId} = require('mongodb')
+const strategies = require('./strategies')
 
 async function start() {
 
@@ -39,63 +39,12 @@ async function start() {
         const oothMongo = new OothMongo(db, ObjectId)
         await ooth.start(app, oothMongo)
 
-        ooth.use('guest', oothGuest())
+        strategies(settings).forEach(strategy => {
+            const {name, methods} = strategy;
+            ooth.use(name, methods)
+        })
 
         const sendMail = mail(settings.mailgun)
-        ooth.use('local', oothLocal({
-            onRegister({email, verificationToken}) {
-                sendMail({
-                    from: settings.mail.from,
-                    to: email,
-                    subject: 'Welcome',
-                    body: `Thank you for registering to our pretty website!`,
-                    html: `Thank you for registering to our pretty website!`,
-                })
-                sendMail({
-                    from: settings.mail.from,
-                    to: email,
-                    subject: 'Verify your email address',
-                    body: `Please verify your email address with the token ${verificationToken}.`,
-                    html: `Please verify your email address with the token ${verificationToken}.`,
-                })
-            },
-            onGenerateVerificationToken({email, verificationToken}) {
-                sendMail({
-                    from: settings.mail.from,
-                    to: email,
-                    subject: 'Verify your email address',
-                    body: `Please verify your email address with the token ${verificationToken}.`,
-                    html: `Please verify your email address with the token ${verificationToken}.`,
-                })
-            },
-            onVerify({email}) {
-                sendMail({
-                    from: settings.mail.from,
-                    to: email,
-                    subject: 'Address verified',
-                    body: `Your email address has been verified.`,
-                    html: `Your email address has been verified.`,
-                })
-            },
-            onForgotPassword({email, passwordResetToken}) {
-                sendMail({
-                    from: settings.mail.from,
-                    to: email,
-                    subject: 'Reset password',
-                    body: `Reset your password with this token ${passwordResetToken}.`,
-                    html: `Reset your password with this token ${passwordResetToken}.`,
-                })
-            },
-            onResetPassword({email}) {
-                sendMail({
-                    from: settings.mail.from,
-                    to: email,
-                    subject: 'Password has been reset',
-                    body: 'Your password has been reset.',
-                    html: 'Your password has been reset.'
-                })
-            }
-        }))
 
         app.listen(settings.port, function() {
             console.info(`Ooth started on port ${settings.port}`)
