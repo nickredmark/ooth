8bc8bb4d40003ae76ff97dcf7636a3aeac31df57
Nick Redmark <nicolamr@gmail.com>
2018-08-21 10:27:42 +0200

    ooth-i18n typescript

---
+++ packages/ooth-i18n/src/index.ts
@@ -0,0 +1,36 @@
+export type Values = {
+  [key: string]: string | number;
+};
+
+export type Translations = {
+  [key: string]: string | Translations;
+};
+
+export function interpolate(s: string, values: Values): string {
+  let res = s;
+  if (values) {
+    for (const key of Object.keys(values)) {
+      res = res.replace(new RegExp(`{${key}}`, 'g'), values[key] as string);
+    }
+  }
+  return res;
+}
+
+export function i18n(translations: Translations, language: string, key: string, values: Values = {}): string {
+  const parts = key.split('.');
+  let current = translations[language];
+  for (const part of parts) {
+    if ((current as Translations)[part] === undefined) {
+      throw Error(`Unknown translation ${language}.${key}.`);
+    }
+    current = (current as Translations)[part];
+  }
+  current = interpolate(current as string, values);
+  return current;
+}
+
+export const getI18n = (translations: Translations, defaultLanguage: string) => (
+  key: string,
+  values: Values,
+  language: string,
+) => i18n(translations, language || defaultLanguage || Object.keys(translations)[0], key, values);
