a1243bcb1cceb17aa3c2a8750e0e8dfe11a51b52
Nick Redmark <nicolamr@gmail.com>
2018-08-25 11:00:39 +0200

    ooth-mongo typescript

---
+++ packages/ooth-mongo/src/index.ts
@@ -0,0 +1,67 @@
+import { Collection, Db, ObjectId } from 'mongodb';
+
+type StrategyValues = {
+  [key: string]: any;
+};
+
+type User = {
+  _id: string;
+  [key: string]: string | StrategyValues;
+};
+
+function prepare(o: any): User {
+  if (o && o._id) {
+    o._id = o._id.toString();
+  }
+  return o;
+}
+
+class OothMongo {
+  private users: Collection;
+
+  constructor(private db: Db) {
+    if (!db) {
+      throw new Error('DB is required.');
+    }
+
+    this.db = db;
+    this.users = this.db.collection('users');
+  }
+
+  public getUserById = async (id: string) => {
+    const user = await this.users.findOne(new ObjectId(id));
+    return prepare(user);
+  };
+
+  public getUser = async (fields: { [key: string]: any }) => {
+    return await prepare(await this.users.findOne(fields));
+  };
+
+  public getUserByValue = async (fields: string[], value: any) => {
+    return prepare(
+      await this.users.findOne({
+        $or: fields.map((field) => ({
+          [field]: value,
+        })),
+      }),
+    );
+  };
+
+  public updateUser = async (id: string, fields: { [key: string]: any }) => {
+    return await this.users.update(
+      {
+        _id: new ObjectId(id),
+      },
+      {
+        $set: fields,
+      },
+    );
+  };
+
+  public insertUser = async (fields: { [key: string]: StrategyValues }) => {
+    const { insertedId } = await this.users.insertOne(fields);
+    return insertedId.toString();
+  };
+}
+
+export default OothMongo;
---
+++ packages/ooth-mongo/test/index.test.ts
@@ -0,0 +1,95 @@
+import { cloneDeep } from 'lodash';
+import { MongoClient } from 'mongodb';
+import MongodbMemoryServer from 'mongodb-memory-server';
+
+import OothMongo from '../src';
+
+let mongoServer;
+let con;
+let oothMongo;
+let db;
+
+// May require additional time for downloading MongoDB binaries
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+const obfuscate = (obj, ...paths) => {
+  const res = cloneDeep(obj);
+  for (const path of paths) {
+    const keys = path.split('.');
+    let current = res;
+    for (const key of keys.slice(0, -1)) {
+      current = current[key];
+    }
+    current[keys[keys.length - 1]] = '<obfuscated>';
+  }
+
+  return res;
+};
+
+describe('ooth-mongo', () => {
+  beforeAll(async () => {
+    mongoServer = new MongodbMemoryServer();
+    const connectionString = await mongoServer.getConnectionString();
+    const dbName = await mongoServer.getDbName();
+    con = await MongoClient.connect(connectionString);
+    db = await con.db(dbName);
+    await db.dropDatabase();
+    oothMongo = new OothMongo(db);
+  });
+
+  afterAll(async () => {
+    await con.close();
+    await mongoServer.stop();
+  });
+
+  afterEach(async () => {
+    await db.dropDatabase();
+  });
+
+  test('can insert user and get it', async () => {
+    const id = await oothMongo.insertUser({
+      foo: 'bar',
+    });
+    expect(typeof id).toBe('string');
+    const user = await oothMongo.getUserById(id);
+    expect(obfuscate(user, '_id')).toMatchSnapshot();
+  });
+
+  test('can update user', async () => {
+    const id = await oothMongo.insertUser({});
+    await oothMongo.updateUser(id, {
+      foo: 'bar',
+    });
+    const user = await oothMongo.getUserById(id);
+    expect(obfuscate(user, '_id')).toMatchSnapshot();
+  });
+
+  test('can get user by value', async () => {
+    const id = await oothMongo.insertUser({
+      foo: {
+        bar: 'baz',
+      },
+      foo2: {
+        bar2: 'baz2',
+      },
+    });
+    const user = await oothMongo.getUserByValue(['foo.bar', 'foo2.bar2'], 'baz2');
+    expect(obfuscate(user, '_id')).toMatchSnapshot();
+  });
+
+  test('can get user by fields', async () => {
+    const id = await oothMongo.insertUser({
+      foo: {
+        bar: 'baz',
+      },
+      foo2: {
+        bar2: 'baz2',
+      },
+    });
+    const user = await oothMongo.getUser({
+      'foo.bar': 'baz',
+      'foo2.bar2': 'baz2',
+    });
+    expect(obfuscate(user, '_id')).toMatchSnapshot();
+  });
+});
