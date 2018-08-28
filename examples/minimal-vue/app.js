87670cb1bc920e59350005f5d8e01d18313fefeb
Nick Redmark <nicolamr@gmail.com>
2018-08-28 05:28:17 +0200

    update minimal examples

--- examples/minimal-vue/app.js
+++ examples/minimal-vue/app.js
@@ -1,98 +1,98 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        user: null,
-        registerUser: {
-            email: "",
-            password: ""
-        },
-        loginUser: {
-            email: "",
-            password: ""
-        }
+  el: "#app",
+  data: {
+    user: null,
+    registerUser: {
+      email: "",
+      password: ""
     },
-
-    methods: {
-
-        checkUser: async function() {
-            const res = await fetch('/auth/status', {
-                credentials: 'include',
-            })
-            const body = await res.json()
-            if (body.status === 'error') {
-                alert(body.message)
-                return
-            }
-            Vue.set(this, 'user', body.user)
-        },
-
-        register: async function(e) {
-            const email = this.registerUser.email
-            const password = this.registerUser.password
-            const res = await fetch('/auth/local/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    email,
-                    password,
-                }),
-                credentials: 'include',
-            })
-            const body = await res.json()
-            if (body.status === 'error') {
-                alert(body.message)
-                return
-            }
-            await this.loginWithEmailPassword(email, password)
-        },
-
-        loginWithEmailPassword: async function(username, password) {
-            const res = await fetch('/auth/local/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    username,
-                    password,
-                }),
-                credentials: 'include',
-            })
-            const body = await res.json()
-            if (body.status === 'error') {
-                alert(body.message)
-                return
-            }
-            Vue.set(this, 'user', body.user)
-            this.clearForm()
-        },
-
-        logout: async function() {
-            const res = await fetch('/auth/logout', {
-                method: 'POST',
-                credentials: 'include',
-            })
-            const body = await res.json()
-            if (body.status === 'error') {
-                alert(body.message)
-                return
-            }
-            Vue.set(this, 'user', null)
-        },
-
-        clearForm: function() {
-            this.registerUser.email = ""
-            this.registerUser.password = ""
-            this.loginUser.email = ""
-            this.loginUser.password = ""
-        }
-    },
-
-    mounted: function() {
-        this.$nextTick(function() {
-            this.checkUser()
-        })
+    loginUser: {
+      email: "",
+      password: ""
     }
-})
+  },
+
+  methods: {
+    checkUser: async function() {
+      const res = await fetch("/auth/user/user", {
+        method: "POST",
+        credentials: "include"
+      });
+      const body = await res.json();
+      if (body.status === "error") {
+        alert(body.message);
+        return;
+      }
+      Vue.set(this, "user", body.user);
+    },
+
+    register: async function(e) {
+      const email = this.registerUser.email;
+      const password = this.registerUser.password;
+      const res = await fetch("/auth/local/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        credentials: "include"
+      });
+      const body = await res.json();
+      if (body.status === "error") {
+        alert(body.message);
+        return;
+      }
+      await this.loginWithEmailPassword(email, password);
+    },
+
+    loginWithEmailPassword: async function(username, password) {
+      const res = await fetch("/auth/local/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        credentials: "include"
+      });
+      const body = await res.json();
+      if (body.status === "error") {
+        alert(body.message);
+        return;
+      }
+      Vue.set(this, "user", body.user);
+      this.clearForm();
+    },
+
+    logout: async function() {
+      const res = await fetch("/auth/session/logout", {
+        method: "POST",
+        credentials: "include"
+      });
+      const body = await res.json();
+      if (body.status === "error") {
+        alert(body.message);
+        return;
+      }
+      Vue.set(this, "user", null);
+    },
+
+    clearForm: function() {
+      this.registerUser.email = "";
+      this.registerUser.password = "";
+      this.loginUser.email = "";
+      this.loginUser.password = "";
+    }
+  },
+
+  mounted: function() {
+    this.$nextTick(function() {
+      this.checkUser();
+    });
+  }
+});
--- examples/minimal/index.js
+++ examples/minimal/index.js
@@ -1,63 +1,68 @@
-const {MongoClient, ObjectId} = require('mongodb')
-const express = require('express')
-const session = require('express-session')
-const {promisify} = require('util')
-const Ooth = require('ooth')
-const oothLocal = require('ooth-local')
-const OothMongo = require('ooth-mongo')
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const session = require("express-session");
+const { promisify } = require("util");
+const { Ooth } = require("ooth");
+const oothLocal = require("ooth-local").default;
+const oothUser = require("ooth-user").default;
+const { OothMongo } = require("ooth-mongo");
 
-const MONGO_HOST = 'mongodb://localhost:27017'
-const MONGO_DB = 'ooth-minimal'
-const HOST = 'http://localhost'
-const PORT = 3000
-const SECRET = 'somesecret'
-const SHARED_SECRET = 'somesharedsecret'
-const OOTH_PATH = '/auth'
+const MONGO_HOST = "mongodb://localhost:27017";
+const MONGO_DB = "ooth-minimal";
+const HOST = "localhost";
+const PORT = 3000;
+const SECRET = "somesecret";
+const OOTH_PATH = "/auth";
 
 const start = async () => {
-    try {
-        const client = await MongoClient.connect(MONGO_HOST)
-        const db = client.db(MONGO_DB)
-    
-        const app = express()
-        app.use(session({
-            name: 'api-session-id',
-            secret: SECRET,
-            resave: false,
-            saveUninitialized: true,
-        }))
+  try {
+    const client = await MongoClient.connect(MONGO_HOST);
+    const db = client.db(MONGO_DB);
+    const app = express();
+    const oothMongo = new OothMongo(db);
+    const ooth = new Ooth({
+      app,
+      backend: oothMongo,
+      sessionSecret: SECRET,
+      path: OOTH_PATH
+    });
 
-        const ooth = new Ooth({
-            sharedSecret: SHARED_SECRET,
-            path: OOTH_PATH,
-        })
-        const oothMongo = new OothMongo(db, ObjectId)
-        await ooth.start(app, oothMongo)
-        ooth.use('local', oothLocal({
-            onRegister({email, verificationToken, _id}) {
-                console.log(`Someone registered.`)
-            },
-            onGenerateVerificationToken({email, verificationToken}) {
-                console.log(`Someone requested a verification email.`)
-            },
-            onVerify({email}) {
-                console.log(`Someone verified their email`)
-            },
-            onForgotPassword({email, passwordResetToken}) {
-                console.log(`Someone forgot their password`)
-            },
-            onResetPassword({email}) {
-                console.log(`Someone reset their password`)
-            },
-        }))
+    oothLocal({
+      ooth
+    });
+    oothUser({
+      ooth
+    });
 
-        app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
+    ooth.on("local", "register", ({ email, verificationToken, _id }) => {
+      console.log(`Someone registered.`);
+    });
+    ooth.on(
+      "local",
+      "generate-verification-token",
+      ({ email, verificationToken }) => {
+        console.log(`Someone requested a verification email.`);
+      }
+    );
+    ooth.on("local", "verify", ({ email }) => {
+      console.log(`Someone verified their email`);
+    });
+    ooth.on("local", "forgot-password", ({ email, passwordResetToken }) => {
+      console.log(`Someone forgot their password`);
+    });
+    ooth.on("local", "reset-password", ({ email }) => {
+      console.log(`Someone reset their password`);
+    });
 
-        await promisify(app.listen)(PORT)
-        console.info(`Online at ${HOST}:${PORT}`)
-    } catch (e) {
-        console.error(e)
-    }
-}
+    app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`));
 
-start()
+    await new Promise((res, rej) =>
+      app.listen(PORT, HOST, e => (e ? rej(e) : res()))
+    );
+    console.info(`Online at ${HOST}:${PORT}`);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
