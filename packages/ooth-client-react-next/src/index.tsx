9b6ccde6825bf8b04c5646d88e495d195aa6d0b5
Nick Redmark <nicolamr@gmail.com>
2018-10-22 11:12:34 +0200

    recreate ooth-client for each request in next server

--- packages/ooth-client-react-next/src/index.tsx
+++ packages/ooth-client-react-next/src/index.tsx
@@ -3,9 +3,42 @@ import { composeInitialProps } from "compose-next";
 import { OothClient, User } from "ooth-client";
 import { OothProvider } from "ooth-client-react";
 
-export default (oothClient: OothClient) => {
+export default (getOothClient: OothClient | (() => OothClient)) => {
+  let commonOothClient: OothClient;
+
+  if (typeof getOothClient !== "function") {
+    console.warn(
+      "SECURITY WARNING: Don't pass OothClient directly to ooth-client-react-next. Instad pass a function that returns a new object. This is needed for SSR."
+    );
+  }
+
+  const getActualOothClient = () => {
+    if (typeof getOothClient !== "function") {
+      return getOothClient;
+    }
+
+    if ((process as any).browser) {
+      if (!commonOothClient) {
+        commonOothClient = getOothClient();
+      }
+
+      return commonOothClient;
+    }
+
+    return getOothClient();
+  };
+
   class OothProviderWithInitialProps extends React.Component<{ initialUser: User | undefined; children: React.Component }> {
+    private oothClient: OothClient;
+
+    constructor(props: any) {
+      super(props);
+      this.oothClient = getActualOothClient();
+    }
+
     public static async getInitialProps(ctx: any): Promise<{ initialUser: User | undefined }> {
+      const oothClient = getActualOothClient();
+
       return {
         initialUser: ctx.req
           ? await oothClient.fetchUser(ctx.req.headers && { cookie: ctx.req.headers.cookie })
@@ -17,7 +50,7 @@ export default (oothClient: OothClient) => {
       const { initialUser, children } = this.props;
 
       return (
-        <OothProvider client={oothClient} initialUser={initialUser}>
+        <OothProvider client={this.oothClient} initialUser={initialUser}>
           {children}
         </OothProvider>
       );
