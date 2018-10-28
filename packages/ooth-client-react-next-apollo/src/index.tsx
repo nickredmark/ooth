69105e995820170b3872f14a0f042b93bbe8ebb3
Nick Redmark <nicolamr@gmail.com>
2018-10-28 22:09:46 +0100

    apollo opts

--- packages/ooth-client-react-next-apollo/src/index.tsx
+++ packages/ooth-client-react-next-apollo/src/index.tsx
@@ -1,7 +1,7 @@
 import 'isomorphic-unfetch';
 
 import { ApolloReducerConfig, InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient } from 'apollo-client';
+import { ApolloClient, ApolloClientOptions } from 'apollo-client';
 import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import * as React from 'react';
@@ -9,7 +9,12 @@ import { ApolloProvider, getDataFromTree } from 'react-apollo';
 
 let client: ApolloClient<any> | null = null;
 
-const getBrowserClient = (uri: string, cacheOpts?: ApolloReducerConfig, initialData?: any) => {
+const getBrowserClient = (
+  uri: string,
+  opts: Partial<ApolloClientOptions<any>>,
+  cacheOpts?: ApolloReducerConfig,
+  initialData?: any,
+) => {
   const link = new HttpLink({
     uri,
     credentials: 'include',
@@ -21,12 +26,14 @@ const getBrowserClient = (uri: string, cacheOpts?: ApolloReducerConfig, initialD
   return new ApolloClient({
     link,
     cache,
+    ...opts,
   });
 };
 
 const getServerClient = (
   uri: string,
   cookies: { [key: string]: string },
+  opts: Partial<ApolloClientOptions<any>>,
   cacheOpts?: ApolloReducerConfig,
   initialData?: any,
 ) => {
@@ -56,17 +63,24 @@ const getServerClient = (
     link,
     cache,
     ssrMode: true,
+    ...opts,
   });
 };
 
-const getClient = (uri: string, cookies: { [key: string]: string }, cacheOpts?: ApolloReducerConfig, initialData?: any) => {
+const getClient = (
+  uri: string,
+  cookies: { [key: string]: string },
+  opts: Partial<ApolloClientOptions<any>>,
+  cacheOpts?: ApolloReducerConfig,
+  initialData?: any,
+) => {
   if (!(process as any).browser) {
     // on server, create a new client for each request
-    return getServerClient(uri, cookies, cacheOpts, initialData);
+    return getServerClient(uri, cookies, opts, cacheOpts, initialData);
   }
   // on client, create singleton
   if (!client) {
-    client = getBrowserClient(uri, cacheOpts, initialData);
+    client = getBrowserClient(uri, opts, cacheOpts, initialData);
   }
   return client;
 };
@@ -74,11 +88,12 @@ const getClient = (uri: string, cookies: { [key: string]: string }, cacheOpts?: 
 export type Options = {
   url: string;
   opts?: ApolloReducerConfig;
+  apolloOpts?: Partial<ApolloClientOptions<any>>;
 };
 
 type Props = { initialData?: any; childProps?: any };
 
-export default ({ url, opts }: Options) => {
+export default ({ url, opts, apolloOpts }: Options) => {
   // tslint:disable-next-line variable-name
   return (Component: React.ComponentType<any> & { getInitialProps?: (ctx: any) => Promise<any> }) =>
     class extends React.Component<Props> {
@@ -87,12 +102,12 @@ export default ({ url, opts }: Options) => {
       constructor(props: Props) {
         super(props);
         // On server, this client won't be doing any work, because all work has been done in getInitialProps
-        this.client = getClient(url, {}, opts, props.initialData);
+        this.client = getClient(url, {}, apolloOpts || {}, opts, props.initialData);
       }
 
       public static async getInitialProps(ctx: any): Promise<{ initialData: any; childProps: any }> {
         const cookies = ctx.req && ctx.req.cookies;
-        const client = getClient(url, cookies, opts);
+        const client = getClient(url, cookies, apolloOpts || {}, opts);
 
         const childProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
 
