5418c010792f66e453bd3061bffa5cf2dbbaf3d2
Davey Brown <daveybrown@users.noreply.github.com>
2019-03-13 19:41:49 +0000

    main functions of ooth-prisma working getUserById etc. only setup for the following fields.
      id: ID! @unique
      email: String @unique
      username: String @unique
      password: String
      verificationToken: String
      verificationTokenExpiresAt: DateTime
      userMeta: [UserMeta]! @relation(name: "UserMetasForUser")

--- examples/standalone-prisma/ooth/index.js
+++ examples/standalone-prisma/ooth/index.js
@@ -1,26 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const { Ooth } = require('ooth');
-const { OothMongo } = require('ooth-mongo');
-const { OothPrisma } = require('./ooth-prisma');
-const oothGuest = require('ooth-guest').default;
-const oothLocal = require('ooth-local').default;
-const oothUser = require('ooth-user').default;
-const oothWs = require('ooth-ws').default;
-const emailer = require('ooth-local-emailer').default;
-const oothJwt = require('ooth-jwt').default;
-const morgan = require('morgan');
-const cors = require('cors');
-const mail = require('./mail');
-const { MongoClient } = require('mongodb');
-const { Prisma } = require('./prisma-client');
-
-// prismadcker ps
+require("dotenv").config();
+const express = require("express");
+const { Ooth } = require("ooth");
+const { OothMongo } = require("ooth-mongo");
+const { OothPrisma } = require("./ooth-prisma");
+const oothGuest = require("ooth-guest").default;
+const oothLocal = require("ooth-local").default;
+const oothUser = require("ooth-user").default;
+const oothWs = require("ooth-ws").default;
+const emailer = require("ooth-local-emailer").default;
+const oothJwt = require("ooth-jwt").default;
+const morgan = require("morgan");
+const cors = require("cors");
+const mail = require("./mail");
+const { MongoClient } = require("mongodb");
+const { Prisma } = require("./prisma-client");
 
 async function start() {
   try {
     const app = express();
-    app.use(morgan('dev'));
+    app.use(morgan("dev"));
     const corsMiddleware = cors({
       origin: process.env.ORIGIN_URL,
       credentials: true,
@@ -32,67 +30,8 @@ async function start() {
     const prisma = new Prisma();
     const oothPrisma = new OothPrisma(prisma);
 
-    // const userByEmail = await prisma.user({
-    //   email: 'hi2@there.com'
-    // });
-
-    // console.log({ userByEmail });
-
     const db = await MongoClient.connect(process.env.MONGO_URL);
     const oothMongo = new OothMongo(db);
-
-    // let devUsers = await oothMongo.users;
-    // // let devGetUserById = await oothMongo.getUserById('1234');
-    // let devGetUserByUniqueField = await oothMongo.getUser({
-    //   'email': 'davey@web-shed.com'
-    // });
-
-    const id = await oothMongo.insertUser({
-      foo: {
-        baz: 'bar'
-      }
-    });
-
-    const user = await oothMongo.getUserById(id);
-    console.log({ id, user });
-
-    const id2 = await oothMongo.insertUser({});
-    await oothMongo.updateUser(id2, {
-      foo2: 'bar2'
-    });
-    const user2 = await oothMongo.getUserById(id2);
-
-    console.log({ id2, user2 });
-
-    await oothMongo.insertUser({
-      foo: {
-        bar: 'baz'
-      },
-      foo2: {
-        bar2: 'baz2'
-      }
-    });
-    const user3 = await oothMongo.getUserByValue(
-      ['foo.bar', 'foo2.bar2'],
-      'baz2'
-    );
-
-    console.log({ user3 });
-
-    await oothMongo.insertUser({
-      foo: {
-        bar: 'baz'
-      },
-      foo2: {
-        bar2: 'baz2'
-      }
-    });
-    const user4 = await oothMongo.getUser({
-      'foo.bar': 'baz',
-      'foo2.bar2': 'baz2'
-    });
-
-    console.log({ user4 });
 
     const ooth = new Ooth({
       app,
@@ -119,7 +58,7 @@ async function start() {
     oothJwt({
       ooth,
       sharedSecret: process.env.SHARED_SECRET,
-      tokenLocation: 'header'
+      tokenLocation: "header"
     });
     oothWs({ ooth });
 
---
+++ examples/standalone-prisma/ooth/ooth-prisma/src/index.ts
@@ -0,0 +1,98 @@
+type StrategyValues = {
+  [key: string]: any;
+};
+
+type User = {
+  _id: string;
+  [key: string]: string | StrategyValues;
+};
+
+function prepare(o: any): User {
+  if (o && !o._id && o.id) {
+    o._id = o.id;
+  }
+  if (o && o._id) {
+    o._id = o._id.toString();
+  }
+  return o;
+}
+
+export class OothPrisma {
+
+  constructor(private prisma: any) {
+    if (!prisma) {
+      throw new Error('DB is required.');
+    }
+
+    this.prisma = prisma;
+  }
+
+  public getUserById = async (id: string) => {
+    try {
+      const user = await this.prisma.user({ id });
+      return prepare(user);
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  public getUser = async (fields: { [key: string]: any }) => {
+    try {
+      const users = await this.prisma.users({ where: fields });
+      if (users.length > 1) {
+        console.log('The getUser query found ' + users.length + ' users. users[0] returned');
+      }
+      return await prepare(users[0]);
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  public getUserByValue = async (fields: string[], value: any) => {
+    try {
+      const users = await this.prisma.users({
+        where: {
+          OR: fields.map((field) => ({
+            [field]: value,
+          })),
+        },
+      });
+      if ( users.length > 1 ) {
+        console.log('The getUserByValue query found ' + users.length + ' users. users[0] returned');
+      }
+      return prepare(users[0]);
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  public updateUser = async (id: string, fields: { [key: string]: any }) => {
+    try {
+      const user = await await this.prisma.updateUser({
+        data: {
+          ...fields,
+        },
+        where: {
+          id,
+        },
+      });
+      return user;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  public insertUser = async (fields: { [key: string]: StrategyValues }) => {
+    try {
+      const { id } = await this.prisma.createUser(fields);
+      return id;
+    } catch (err) {
+      console.error(err);
+      return null; 
+    }
+  };
+}
